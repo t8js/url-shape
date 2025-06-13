446bcc32304246dd26944a15d644d3535f24a6ed
axtk <axtkx@inbox.ru>
2025-06-13 09:39:12 +0300

    remove unused

--- src/types/UnpackedURLSchema.ts
+++ src/types/UnpackedURLSchema.ts
@@ -1,18 +1,6 @@
 import type {URLSchema} from './URLSchema';
 import type {UnpackedSchema} from './UnpackedSchema';
 
-// export type UnpackedURLSchema<T extends URLSchema> = T extends null
-//     ? void
-//     : {
-//           params?: NonNullable<T> extends {params: unknown}
-//               ? UnpackedSchema<NonNullable<T>['params']>
-//               : never;
-//           query?: NonNullable<T> extends {query: unknown}
-//               ? UnpackedSchema<NonNullable<T>['query']>
-//               : never;
-//           // hash?: UnpackedSchema<NonNullable<T>['hash'], string>;
-//       };
-
 export type UnpackedURLSchema<T extends URLSchema> = T extends null
     ? undefined
     : NonNullable<T> extends {params: unknown; query: unknown}
