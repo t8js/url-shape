930d078f3d4c0eca70ab129b77017c24407f00c6
axtk <axtkx@inbox.ru>
2025-06-14 16:29:57 +0300

    add constraints to url schema

--- src/types/URLSchema.ts
+++ src/types/URLSchema.ts
@@ -1,7 +1,7 @@
 import type {Schema} from 'unpack-schema';
 
 export type URLSchema = {
-    params?: Schema;
-    query?: Schema;
+    params?: Schema<Record<string, unknown>>;
+    query?: Schema<Record<string, unknown>>;
     // hash?: Schema;
 } | null;
