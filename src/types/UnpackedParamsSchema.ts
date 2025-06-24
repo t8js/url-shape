cf5192832d44454974ef09285782310c211b5a34
axtk <axtkx@inbox.ru>
2025-06-24 13:45:42 +0300

    update params and query types

--- src/types/UnpackedParamsSchema.ts
+++ src/types/UnpackedParamsSchema.ts
@@ -1,11 +1,14 @@
-import type {UnpackedSchema} from 'unpack-schema';
+import type {Schema, UnpackedSchema} from 'unpack-schema';
 import type {URLMapSchema} from '../types/URLMapSchema';
+
+type R = Schema<Record<string, unknown>>;
+type RX = Schema<Record<string, unknown> | undefined>;
 
 export type UnpackedParamsSchema<
     S extends URLMapSchema,
     P extends keyof S = keyof S,
 > = S extends null
     ? Record<string, string | string[] | undefined>
-    : NonNullable<S>[P] extends {params: unknown}
+    : NonNullable<S>[P] extends {params?: R | RX}
       ? UnpackedSchema<NonNullable<NonNullable<S>[P]>['params']>
       : Record<string, never>;
--- src/types/UnpackedQuerySchema.ts
+++ src/types/UnpackedQuerySchema.ts
@@ -9,8 +9,6 @@ export type UnpackedQuerySchema<
     P extends keyof S = keyof S,
 > = S extends null
     ? Record<string, string | string[]>
-    : NonNullable<S>[P] extends {query?: RX}
-      ? Record<string, never>
-      : NonNullable<S>[P] extends {query: R}
-        ? UnpackedSchema<NonNullable<NonNullable<S>[P]>['query']>
-        : Record<string, never>;
+    : NonNullable<S>[P] extends {query?: R | RX}
+      ? UnpackedSchema<NonNullable<NonNullable<S>[P]>['query']>
+      : Record<string, never>;
